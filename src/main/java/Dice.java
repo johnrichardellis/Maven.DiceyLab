0d92e98b3bbf76975ff058bca009d3aa8fbeac8f
Ellis John <ellisjohn@zipcodembpchuck.myfiosgateway.com>
2021-11-20 11:34:00 -0500

    lab

--- src/main/java/Dice.java
+++ src/main/java/Dice.java
@@ -2,18 +2,39 @@ import java.util.concurrent.ThreadLocalRandom;
 
 public class Dice {
 
-    // method to take in n number of dies
-    // use random number to simulate each dice
-    public int throwValue (int numberOfDie) {
-        int value = ThreadLocalRandom.current().nextInt(1, 7);
-        return value;
-    };
+    private int numOfDice;
 
-    public int tossAndSum () {
+   public Dice(int numOfDice) {
+       // sets value to parameter given
+       this.numOfDice = numOfDice;
+   }
 
-    }
+   public int tossAndSum() {
+       // create counter for sum
+       int sumOfDice = 0;
+       // loop through however many dice, each time adding total to current sum to get total sum
+       for (int i = 0; i < numOfDice; i++) {
+            sumOfDice = sumOfDice + rollDice();
+       }
+       return sumOfDice;
+   }
+
+   public int rollDice() {
+
+       // use math.random to generate random 0 through 5, then adding 1 to get 1-6 instead
+       return (int) ((Math.random() * 6) + 1);
+   }
 
 
+
+   public int getNumOfDice() {
+       return numOfDice;
+   }
+
+
+   public void setNumOfDice(int numOfDice) {
+       this.numOfDice = numOfDice;
+   }
 
 
 }
